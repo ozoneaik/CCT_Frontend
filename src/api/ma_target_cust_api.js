6e05655e731332c74fdd072e4d52745ea7b09dfe
phuwadech <aofphuwadech@gmail.com>
2024-06-21 12:02:49 +0700

    Create Main.jsx Success

--- src/api/ma_target_cust_api.js
+++ src/api/ma_target_cust_api.js
@@ -13,3 +13,27 @@ export function getMaTargetCustApi(target=1,target_month,onPassed) {
         }
     );
 }
+
+export function getMaTargetBootApi(target=1,target_month,onPassed) {
+    console.log(target_month)
+    axiosClient.get( `${prefix}/booth_list/${target_month}/${target}`,)
+        .then(({data, status}) => {
+            onPassed(data, status)
+        }).catch((error) => {
+            let Error = error.response;
+            onPassed(Error.data, Error.status);
+        }
+    );
+}
+
+export function getMaTargetTrainApi(target=1,target_month,onPassed) {
+    console.log(target_month)
+    axiosClient.get( `${prefix}/train_list/${target_month}/${target}`,)
+        .then(({data, status}) => {
+            onPassed(data, status)
+        }).catch((error) => {
+            let Error = error.response;
+            onPassed(Error.data, Error.status);
+        }
+    );
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,94 +7,129 @@ import {Thai} from "flatpickr/dist/l10n/th.js"
 import {useEffect, useState} from "react";
 import CardComponent from "../components/CardComponent.jsx";
 import {Link} from "react-router-dom";
-import {getMaTargetCustApi} from "../api/ma_target_cust_api.js";
+import {getMaTargetBootApi, getMaTargetCustApi, getMaTargetTrainApi} from "../api/ma_target_cust_api.js";
 import Loading from "../components/Loading.jsx";
 
 function Main() {
     const [DateTime, setDateTime] = useState('');
     const [targetCusts, setTargetCusts] = useState([]);
     const [targetCustTotal, setTargetCustTotal] = useState(0);
+    const [targetBoothTotal, setTargetBoothTotal] = useState(0);
+    const [targetTrainTotal, setTargetTrainTotal] = useState(0);
     const [custAllTotal, setCustAllTotal] = useState(0);
-
     const [showLoading, setShowLoading] = useState(true);
 
 
     useEffect(() => {
-        // setDateTime(formatDate(new Date()));
         let savedDateTime = localStorage.getItem('DateTime') || formatDate(new Date());
+        localStorage.setItem('DateTime', savedDateTime);
         setDateTime(savedDateTime);
         DATEPICKER(savedDateTime);
-        list(1);
+        onClickList(0, savedDateTime)
+        onClickList(1, savedDateTime)
+
 
     }, [])
 
     const DATEPICKER = (savedDateTime) => {
-
-        const currentDate = new Date();
-        const currentYear = currentDate.getFullYear();
-        const currentMonth = currentDate.getMonth(); // เดือน 0-11
-
-        // กำหนดวันที่เริ่มต้นและสิ้นสุด
-        const minDate = new Date(currentYear, 0, 1); // 1 มกราคม ของปีนี้
-        const maxDate = new Date(currentYear, currentMonth + 1, 1); // เดือนถัดจากปัจจุบัน
-
-
         flatpickr("#myID", {
             "locale": Thai,
             defaultDate: savedDateTime,
-            enable: [
-                {
-                    from: minDate,
-                    to: maxDate
-                }
-            ],
+            maxDate: new Date(),
             plugins: [
                 new monthSelectPlugin({
-                    shorthand: true, //defaults to false
-                    dateFormat: "m/Y", //defaults to "F Y"
-                    altFormat: "M/Y", //defaults to "F Y"
-                    theme: "light" // defaults to "light"
+                    shorthand: true,
+                    dateFormat: "m/Y",
+                    altFormat: "M/Y",
+                    theme: "light"
                 })
             ], onChange: (selectedDates, dateStr) => {
                 setDateTime(dateStr)
                 localStorage.setItem('DateTime', dateStr);
-                onClickList(1,dateStr)
+                onClickList(0, dateStr)
             }
         });
     }
 
     const formatDate = (date) => {
-        const month = ('0' + (date.getMonth() + 1)).slice(-2); // เพิ่ม 1 เพราะ getMonth() คืนค่าตั้งแต่ 0-11
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
         const year = date.getFullYear();
         return `${month}/${year}`;
     };
 
-    const list = (target) => {
-        let target_month = new Date();
-        target_month = target_month.getFullYear() + '-' +
-            ('0' + (target_month.getMonth() + 1)).slice(-2) + '-' + '01'
+    const onClickList = (target, TarMonth = DateTime) => {
+        setTargetCusts([]);
+        let [month, year] = TarMonth.split('/');
+        month = parseInt(month, 10);
+        year = parseInt(year, 10);
+        if (month === 12) {
+            month = 1;
+            year += 1;
+        } else {
+            month += 1;
+        }
+        setDateTime(`${('0' + month).slice(-2)}/${year}`);
+        let target_month = `${year}-${('0' + month).slice(-2)}-01`;
+        console.log('on Click List >> ', target_month);
         getMaTargetCustApi(target, target_month, (data, status) => {
-            console.log(data)
+            console.log(data);
             if (status === 200) {
                 setCustAllTotal(data.CustAllTotal)
+                if (target === 1) {
+                    setTargetCustTotal(data.TargetCustTotal)
+                }else{
+                    setTargetBoothTotal(data.BoothAllTotal)
+                    setTargetTrainTotal(data.TrainAllTotal)
+                }
                 setTargetCusts(data.TargetCust)
-                setTargetCustTotal(data.TargetCustTotal)
             }
-        })
+        });
     }
 
-    const onClickList = (target,TerMonth=DateTime) => {
+    const onClickBooth = (target, TerMonth = DateTime) => {
         setTargetCusts([]);
-        let [month, year] = TerMonth.split('/'); // แยกปีและเดือน
-        let target_month = `${year}-${month}-01`; // รวมปี, เดือน, และวันที่
-        console.log(target_month); // จะได้ผลลัพธ์เป็น '2024-06-01'
-        getMaTargetCustApi(target, target_month, (data, status) => {
-            console.log(data)
-            if (status == 200) {
+        let [month, year] = TerMonth.split('/');
+        month = parseInt(month, 10);
+        year = parseInt(year, 10);
+        if (month === 12) {
+            month = 1;
+            year += 1;
+        } else {
+            month += 1;
+        }
+        setDateTime(`${('0' + month).slice(-2)}/${year}`);
+        let target_month = `${year}-${('0' + month).slice(-2)}-01`;
+        console.log('on Click List >> ' , target_month);
+        getMaTargetBootApi(target, target_month, (data, status) => {
+            console.log(data);
+            if (status === 200) {
                 setTargetCusts(data.TargetCust)
             }
-        })
+        });
     }
+
+    const onClickTrain = (target, TerMonth = DateTime) => {
+        setTargetCusts([]);
+        let [month, year] = TerMonth.split('/');
+        month = parseInt(month, 10);
+        year = parseInt(year, 10);
+        if (month === 12) {
+            month = 1;
+            year += 1;
+        } else {
+            month += 1;
+        }
+        setDateTime(`${('0' + month).slice(-2)}/${year}`);
+        let target_month = `${year}-${('0' + month).slice(-2)}-01`;
+        console.log('on Click List >> ' , target_month);
+        getMaTargetTrainApi(target, target_month, (data, status) => {
+            console.log(data);
+            if (status === 200) {
+                setTargetCusts(data.TargetCust)
+            }
+        });
+    }
+
 
     return (
         <Content>
@@ -103,13 +138,18 @@ function Main() {
                     <div className={'col-12'}>
                         <div className={'d-flex justify-content-end'}>
                             <div className="input-group mb-3" style={{maxWidth: 300}}>
-                                <input type="text" id={'myID'} className="form-control" style={{background: '#fff'}}
+                                <input type="text"
+                                       id={'myID'}
+                                       className="form-control"
+                                       style={{background: '#fff'}}
                                        placeholder="Recipient's username"
-                                       aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                                       aria-label="Recipient's username"
+                                       aria-describedby="basic-addon2"
+                                />
                                 <div className="input-group-append">
-                                <span className="input-group-text" id="basic-addon2">
-                                    <i className="fa-solid fa-calendar-days"></i>
-                                </span>
+                                    <span className="input-group-text" id="basic-addon2">
+                                        <i className="fa-solid fa-calendar-days"></i>
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -117,22 +157,22 @@ function Main() {
                 </div>
 
                 <div className={'row'}>
-                    <CardComponent title={'จำนวนร้านค้าทั้งหมด'} background={'bg-primary'} icon={'fa-shop'}
-                                   val={custAllTotal} onClick={() => onClickList(0)}/>
-                    <CardComponent title={'จำนวนร้านค้าเป้าหมาย'} background={'bg-warning'} icon={'fa-location-dot'}
-                                   val={targetCustTotal} onClick={() => onClickList(1)}/>
-                    <CardComponent title={'จำนวนร้านค้าออกบูท'} background={'bg-success'} icon={'fa-person-booth'}
-                                   val={5}/>
-                    <CardComponent title={'จำนวนร้านค้าอบรม'} background={'bg-info'} icon={'fa-square-poll-vertical'}
-                                   val={10}/>
+                    <CardComponent title={'จำนวนร้านค้าทั้งหมด'} background={'bg-primary'} icon={'fa-shop'} val={custAllTotal}
+                                   onClick={() => onClickList(0, localStorage.getItem('DateTime'))}/>
+                    <CardComponent title={'จำนวนร้านค้าเป้าหมาย'} background={'bg-warning'} icon={'fa-location-dot'} val={targetCustTotal}
+                                   onClick={() => onClickList(1, localStorage.getItem('DateTime'))}/>
+                    <CardComponent title={'จำนวนร้านค้าออกบูท'} background={'bg-success'} icon={'fa-person-booth'} val={targetBoothTotal}
+                                   onClick={()=>onClickBooth(1, localStorage.getItem('DateTime'))}/>
+                    <CardComponent title={'จำนวนร้านค้าอบรม'} background={'bg-info'} icon={'fa-square-poll-vertical'} val={targetTrainTotal}
+                                   onClick={()=>onClickTrain(1, localStorage.getItem('DateTime'))}/>
                 </div>
             </div>
             <div className={'row'}>
                 <div className={'col-md-12'}>
                     <div className={'card card-danger card-outline'}>
                         <div className={'card-body'}>
-                            <div className={'table-responsive'}
-                                 style={{maxHeight: '69dvh', overflowY: 'auto'}}>
+                            <h6 className={'text-bold mb-3'}>เดือนเป้าหมาย {DateTime}</h6>
+                            <div className={'table-responsive'} style={{maxHeight: '69dvh', overflowY: 'auto'}}>
                                 <table className={'table table-bordered table-striped'}>
                                     <thead className={'thead-sticky'}>
                                     <tr>
@@ -157,9 +197,7 @@ function Main() {
                                                 <td>
                                                     <div className={'d-flex justify-content-center align-items-center'}>
                                                         <p className={'mb-0 mr-3'}>{parseFloat(TargetCust.target_sale).toLocaleString()}</p>
-                                                        <Link
-                                                            to={`/TargetShops/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`}
-                                                            className={'btn btn-xs btn-primary'}>
+                                                        <Link to={`/TargetShops/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`} className={'btn btn-xs btn-primary'}>
                                                             <i className="fa-solid fa-pen-to-square"></i>
                                                         </Link>
                                                     </div>
@@ -170,9 +208,7 @@ function Main() {
                                                 <td>
                                                     <div className={'d-flex justify-content-center align-items-center'}>
                                                         <p className={'mb-0 mr-3'}>{parseFloat(TargetCust.sku_count).toLocaleString()}</p>
-                                                        <Link
-                                                            to={`RepeatOrders/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`}
-                                                            className={'btn btn-xs btn-primary'}>
+                                                        <Link to={`RepeatOrders/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`} className={'btn btn-xs btn-primary'}>
                                                             <i className="fa-solid fa-pen-to-square"></i>
                                                         </Link>
                                                     </div>
@@ -180,9 +216,7 @@ function Main() {
                                                 <td>
                                                     <div className={'d-flex justify-content-center align-items-center'}>
                                                         <p className={'mb-0 mr-3'}>{TargetCust.new_sku_count}</p>
-                                                        <Link
-                                                            to={`NewProducts/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`}
-                                                            className={'btn btn-xs btn-primary'}>
+                                                        <Link to={`NewProducts/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`} className={'btn btn-xs btn-primary'}>
                                                             <i className="fa-solid fa-pen-to-square"></i>
                                                         </Link>
                                                     </div>
@@ -190,9 +224,7 @@ function Main() {
                                                 <td>
                                                     <div className={'d-flex justify-content-center align-items-center'}>
                                                         <p className={'mb-0 mr-3'}>{TargetCust.sku_pro_count}</p>
-                                                        <Link
-                                                            to={`/FeaturePromotion/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`}
-                                                            className={'btn btn-xs btn-primary'}>
+                                                        <Link to={`/FeaturePromotion/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`} className={'btn btn-xs btn-primary'}>
                                                             <i className="fa-solid fa-pen-to-square"></i>
                                                         </Link>
                                                     </div>
@@ -200,9 +232,7 @@ function Main() {
                                                 <td>
                                                     <div className={'d-flex justify-content-center align-items-center'}>
                                                         <p className={'mb-0 mr-3'}>{TargetCust.sku_booth_count}</p>
-                                                        <Link
-                                                            to={`Booths/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`}
-                                                            className={'btn btn-xs btn-primary'}>
+                                                        <Link to={`Booths/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`} className={'btn btn-xs btn-primary'}>
                                                             <i className="fa-solid fa-pen-to-square"></i>
                                                         </Link>
                                                     </div>
@@ -210,9 +240,7 @@ function Main() {
                                                 <td>
                                                     <div className={'d-flex justify-content-center align-items-center'}>
                                                         <p className={'mb-0 mr-3'}>{TargetCust.sku_train_count}</p>
-                                                        <Link
-                                                            to={`Training/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`}
-                                                            className={'btn btn-xs btn-primary'}>
+                                                        <Link to={`Training/${DateTime}/${TargetCust.custid}/${TargetCust.custname}`} className={'btn btn-xs btn-primary'}>
                                                             <i className="fa-solid fa-pen-to-square"></i>
                                                         </Link>
                                                     </div>
@@ -237,7 +265,6 @@ function Main() {
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </Content>
